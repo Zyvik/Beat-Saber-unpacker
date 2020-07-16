3bdab3c01ab94e5fe6623c20407ae874ed8ad2c9
Pawel Gawrych <pawel.gawrych@protonmail.com>
2020-07-16 17:24:21 +0200

    Implement songs name cleaning

--- unzip.py
+++ unzip.py
@@ -1,44 +1,56 @@
-from os import listdir, remove, rename
-import zipfile
+import os
 import json
+import zipfile
 
 
-def get_zip_files_list(path=None):
-    """Gets file names without .zip extensions"""
-    files_list = listdir(path)
-    return [f[:-4] for f in files_list if f[-4:] == '.zip']
+def get_beatmaps_file_names(path=None):
+    """Returns beatmap's file names without .zip extensions."""
+    files_list = os.listdir(path)
+    return [f[:-4] for f in files_list if f[-4:] == '.zip' and len(f) == 44]
 
 
 def extract_files(file_list):
     for file in file_list:
-        with zipfile.ZipFile(f"{file}.zip", 'r') as zip:
-            zip.extractall(f"{file}/")
+        # if directory doesn't exist - extract all files
+        if not os.path.isdir(f"{file}"):
+            with zipfile.ZipFile(f"{file}.zip", 'r') as zip:
+                zip.extractall(f"{file}/")
 
 
 def get_song_name(file):
     with open(f"{file}/info.dat", "r") as info:
         info_dict = json.load(info)
-        name = info_dict.get('_songName')
-        author = info_dict.get('_songAuthorName')
+        name = info_dict.get('_songName', 'Unknown_author')
+        author = info_dict.get('_songAuthorName', 'Unknown_name')
         return f"{author} - {name}"
+
+
+def clean_song_name(song_name):
+    """Removes forbiden characters from song name"""
+    invalid_chars = '<>:;[]\"/\\|?*'
+    for character in invalid_chars:
+        song_name = song_name.replace(character, '')
+    return song_name
 
 
 def rename_new_folders(file_list):
     for path in file_list:
         new_name = get_song_name(path)
-        rename(path, new_name)
+        clean_name = clean_song_name(new_name)
+        os.rename(path, clean_name)  # Can throw an exception
 
 
 def remove_zip_files(file_list):
     for file in file_list:
-        remove(f"{file}.zip")
+        os.remove(f"{file}.zip")
 
 
 def main():
-    new_files = get_zip_files_list()
+    new_files = get_beatmaps_file_names()
     extract_files(new_files)
     rename_new_folders(new_files)
     remove_zip_files(new_files)
 
 
-main()
+if __name__ == '__main__':
+    main()
